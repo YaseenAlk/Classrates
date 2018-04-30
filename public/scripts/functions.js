12cfb4f3bc816bfee76159f2b276d611905202d7
YaseenAlk <alkhafaji.yaseen@gmail.com>
2018-04-30 17:50:11 -0400

    Initial commit

--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -1,7 +1,7 @@
 function submitComment(commentControl) {
     var comments = document.getElementsByName(commentControl)[0].value;
-    var subscriptionKey = "TEXT_ANALYTICS_API_KEY";
-    var url = "TEXT_ANALYTICS_ENDPOINT/sentiment";
+    var subscriptionKey = "9c774d9208b346069edc187a34158089";
+    var url = "https://westcentralus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment";
 
     var payload = '{ "documents": [ { "language": "en-US", "id": "1", "text": "' + comments + '" }]}';
 
